7cf910857c6c320e2b36073ac872d5cce3e24820
poirazis <poirazis@gmail.com>
2025-07-03 18:50:47 +0300

    wip

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,9 +1,6 @@
 <script>
   import { getContext, onDestroy } from "svelte";
-  import CellBoolean from "../../bb_super_components_shared/src/lib/SuperTableCells/CellBoolean.svelte";
-  import SuperFieldLabel from "../../bb_super_components_shared/src/lib/SuperFieldLabel/SuperFieldLabel.svelte";
-  import "../../bb_super_components_shared/src/lib/SuperFieldsCommon.css";
-  import "../../bb_super_components_shared/src/lib/SuperTableCells/CellCommon.css";
+  import { SuperField, CellBoolean } from "@poirazis/supercomponents-shared";
 
   const { styleable } = getContext("sdk");
   const component = getContext("component");
@@ -16,7 +13,7 @@
   const groupDisabled = getContext("field-group-disabled");
   const formApi = formContext?.formApi;
 
-  export let field;
+  export let field = "Boolean Field";
   export let controlType;
   export let role = "formInput";
   export let labelPosition = "fieldGroup";
@@ -66,6 +63,7 @@
   });
 
   $: value = fieldState?.value;
+  $: error = fieldState?.error;
 
   $: cellOptions = {
     defaultValue,
@@ -82,12 +80,8 @@
     ...$component.styles,
     normal: {
       ...$component.styles.normal,
-      "flex-direction": labelPos == "left" ? "row" : "column",
-      "align-items": "stretch",
-      gap: labelPos == "left" ? "0.5rem" : "0rem",
       "grid-column": span < 7 ? "span " + span : "span " + groupColumns * 6,
-      "--label-width":
-        labelPos == "left" ? (labelWidth ? labelWidth : "6rem") : "auto",
+      flex: span > 6 ? "auto" : "none",
     },
   };
 
@@ -105,23 +99,13 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 <div use:styleable={$component.styles}>
-  <div class="superField" class:left-label={labelPos == "left"}>
-    <SuperFieldLabel
-      {labelPos}
-      {labelWidth}
-      {label}
-      {helpText}
-      error={fieldState?.error}
+  <SuperField {labelPos} {labelWidth} {field} {label} {error} {helpText}>
+    <CellBoolean
+      bind:cellState
+      {cellOptions}
+      {value}
+      {fieldSchema}
+      on:change={(e) => handleChange(e.detail)}
     />
-
-    <div class="inline-cells">
-      <CellBoolean
-        bind:cellState
-        {cellOptions}
-        {value}
-        {fieldSchema}
-        on:change={(e) => handleChange(e.detail)}
-      />
-    </div>
-  </div>
+  </SuperField>
 </div>
