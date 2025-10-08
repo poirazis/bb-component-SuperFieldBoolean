3a64d9c70646249ce9c3cd4e4d8466e50d364317
poirazis <poirazis@gmail.com>
2025-10-08 16:50:20 +0300

    wip

--- src/Component.svelte
+++ src/Component.svelte
@@ -91,6 +91,7 @@
           : $component.styles.normal.display,
       opacity: invisible && $builderStore.inBuilder ? 0.6 : 1,
       "grid-column": groupColumns ? `span ${span}` : "span 1",
+      overflow: "hidden",
     },
   };
 
